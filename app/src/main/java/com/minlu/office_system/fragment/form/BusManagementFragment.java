c3120b599f1ba521b147f80172b3d2ce93408217
ZHQ <sfgjys@sina.com>
2017-03-21 10:17:51 +0800

    加上车辆管理

--- app/src/main/java/com/minlu/office_system/fragment/form/BusManagementFragment.java
+++ app/src/main/java/com/minlu/office_system/fragment/form/BusManagementFragment.java
@@ -6,6 +6,7 @@ import com.minlu.baselibrary.base.ContentPage;
 import com.minlu.baselibrary.util.ViewsUitls;
 import com.minlu.office_system.R;
 import com.minlu.office_system.activity.FormActivity;
+import com.minlu.office_system.customview.EditTextItem;
 import com.minlu.office_system.fragment.form.formPremise.FormFragment;
 
 import java.util.ArrayList;
@@ -32,7 +33,30 @@ public class BusManagementFragment extends FormFragment {
 
         View inflate = ViewsUitls.inflate(R.layout.form_bus_management);
 
+        initView(inflate);
+
         return inflate;
+    }
+
+    private void initView(View inflate) {
+        EditTextItem title = (EditTextItem) inflate.findViewById(R.id.form_bus_management_title);
+        title.setEditText("出差");
+        EditTextItem office = (EditTextItem) inflate.findViewById(R.id.form_bus_management_office);
+        office.setEditText("宣传部");
+        EditTextItem busNumber = (EditTextItem) inflate.findViewById(R.id.form_bus_management_bus_number);
+        busNumber.setEditText("苏D G23098F");
+
+        EditTextItem startTime = (EditTextItem) inflate.findViewById(R.id.form_bus_management_start_time);
+        startTime.setEditText("2017-6-26");
+
+        EditTextItem destination = (EditTextItem) inflate.findViewById(R.id.form_bus_management_destination);
+        destination.setEditText("湖北省武汉市洪山区珞喻路129号---武汉大学(信息学部)");
+
+        EditTextItem cause = (EditTextItem) inflate.findViewById(R.id.form_bus_management_cause);
+        cause.setEditText("本来就因为有事情才会出差的——带着目的性的出差，而不是出去了才想起应该做什么。出差都是为了公司的事务，有时候可能是谈什么项目、有时候是找某个客户处理什么事情，可能每一次的出差都带着不同的目的。");
+
+        ViewsUitls.setWidthFromTargetView(title.getCustomEditTextLeft(), busNumber.getCustomEditTextLeft());
+        ViewsUitls.setWidthFromTargetView(title.getCustomEditTextLeft(), destination.getCustomEditTextLeft());
     }
 
     @Override
