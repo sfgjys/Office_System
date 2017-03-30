f9f4287102dc411c23a51471982ecee9350de63b
ZHQ <sfgjys@sina.com>
2017-03-30 13:39:00 +0800

    添加注销功能

--- app/src/main/java/com/minlu/office_system/fragment/SettingFragment.java
+++ app/src/main/java/com/minlu/office_system/fragment/SettingFragment.java
@@ -1,11 +1,15 @@
 package com.minlu.office_system.fragment;
 
+import android.content.Intent;
 import android.view.View;
 
 import com.minlu.baselibrary.base.BaseFragment;
 import com.minlu.baselibrary.base.ContentPage;
+import com.minlu.baselibrary.util.SharedPreferencesUtil;
 import com.minlu.baselibrary.util.ViewsUitls;
 import com.minlu.office_system.R;
+import com.minlu.office_system.StringsFiled;
+import com.minlu.office_system.activity.LoginActivity;
 import com.minlu.office_system.customview.SettingItem;
 
 import java.util.ArrayList;
@@ -49,6 +53,9 @@ public class SettingFragment extends BaseFragment implements View.OnClickListene
             case R.id.setting_about_us:
                 break;
             case R.id.setting_logout:
+                SharedPreferencesUtil.saveBoolean(ViewsUitls.getContext(), StringsFiled.IS_AUTO_LOGIN, false);
+                getActivity().startActivity(new Intent(ViewsUitls.getContext(), LoginActivity.class));
+                getActivity().finish();// TODO 这里只需要关闭一个MainActivity界面就行，以后界面开启有改动这里也记得改下
                 break;
         }
     }
